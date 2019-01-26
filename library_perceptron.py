c91d409f4ab19a872834bec07d8ba5f3cd6f59a4
Chris Maltais <chris.maltais@hotmail.ca>
2019-01-26 17:50:52 -0500

    ♻️ Refactor code and add file creation for output

--- library_perceptron.py
+++ library_perceptron.py
@@ -1,53 +1,66 @@
 from sklearn.linear_model import Perceptron
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 import numpy as np
 import csv
+import os
 
-# Get train data: features_train and target_train
-with open('data/testSeeds.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    features_train = []
-    target_train = []
-    for row in readCSV:
-        row_floats = [float(feature) for feature in row[:-1]]
-        features_train.append(row_floats) # First 7 features
-        target_train.append(int(row[len(row) - 1])) # Labels
+def get_features_and_target_data(file):
+    with open(file) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        features = []
+        targets = []
+        for row in readCSV:
+            row_floats = [float(feature) for feature in row[:-1]]
+            features.append(row_floats) # First 7 features
+            targets.append(int(row[len(row) - 1])) # Labels
+        return features, targets
 
-# Get test data: features_test and target_test
-with open('data/trainSeeds.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    features_test = []
-    target_test = []
-    for row in readCSV:
-        row_floats = [float(feature) for feature in row[:-1]]
-        features_test.append(row_floats) # First 7 features
-        target_test.append(int(row[len(row) - 1])) # Labels 
+if __name__ == "__main__":
+    features_train, target_train = get_features_and_target_data('data/trainSeeds.csv')
+    features_test, target_test = get_features_and_target_data('data/testSeeds.csv')
 
-# Standard Scalar is used to standardize data
-sc = StandardScaler()
+    # Standard Scalar is used to standardize data
+    sc = StandardScaler()
 
-# Train the scaler on the training data
-sc.fit(features_train)
+    # Train the scaler on the training data
+    sc.fit(features_train)
 
-# Apply scaler to feature training data
-features_train_std = sc.transform(features_train)
+    # Apply scaler to feature training data
+    features_train_std = sc.transform(features_train)
 
-# Apply scaler to feature test data
-features_test_std = sc.transform(features_test)
+    # Apply scaler to feature test data
+    features_test_std = sc.transform(features_test)
 
-# Create perceptron object with 40 epochs and learning rate of 0.1
-perceptron = Perceptron(max_iter=1000, eta0=0.1)
+    # Create perceptron object with 40 epochs and learning rate of 0.1
+    perceptron = Perceptron(max_iter=1000, eta0=0.1)
 
-# Train perceptron
-perceptron.fit(features_train_std,target_train)
+    # Train perceptron
+    perceptron.fit(features_train_std,target_train)
 
-# Run the trained perceptron on the test wheat data
-target_prediction = perceptron.predict(features_test_std)
+    # Run the trained perceptron on the test wheat data
+    target_prediction = perceptron.predict(features_test_std)
 
-print('Accuracy: %.2f' % accuracy_score(target_test, target_prediction))
-print('Train accuracy: %.2f' % perceptron.score(features_train_std, target_train))
-print('Test accuracy: %.2f' % perceptron.score(features_test_std, target_test))
+    training_accuracy = perceptron.score(features_train_std, target_train) * 100
+    testing_accuracy = perceptron.score(features_test_std, target_test) * 100
+
+    confusion_matrix_results = confusion_matrix(target_test, target_prediction)
+
+    names = ['Kama', 'Rosa', 'Canadian']
+    classification_report_results = classification_report(target_test, target_prediction, target_names=names)
+
+    filename = 'results/libraryPerceptronResults.txt'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w+') as f:
+        f.write('Tool: SciKit Learn\n\n')
+        f.write('Training Set Accuracy: %.2f \n' % training_accuracy)
+        f.write('Testing Set Accuracy: %.2f \n\n' % testing_accuracy)
+        f.write('Confusion Matrix: \n')
+        f.write(np.array2string(confusion_matrix_results))
+        f.write('\n\nClassification Report: \n')
+        f.write(classification_report_results)
 
 
 
